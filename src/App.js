96ac14bc4dbfc183ad5e3b5dcf59b93ea2d997ae
liu xing <chn.liuxing@gmail.com>
2018-10-05 01:01:19 +0800

    chore: use hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import routes from './router'
 import './App.scss'
